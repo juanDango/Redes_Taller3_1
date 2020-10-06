541360a3e98c3471a99d42825ce00c32fd0c4195
Sofia Alvarez <ms.alvarezl@uniandes.edu.co>
2020-10-06 00:01:50 -0500

    Tenemos hasta que se van a enviar los archivos :)

--- Cliente/client2.py
+++ Cliente/client2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import socket
+import struct
 
 TCP_IP = 'localhost'
 TCP_PORT = 9001
@@ -9,6 +10,27 @@ BEG_RECV = b'BEG_RECV'
 OK = b'OK'
 FILE_OPTS = b'FILE_OPTS'
 FILENAMES_SENT = b'FILENAMES_SENT'
+ERR = b'ERR'
+
+def sendOneMessage(socket, data):
+    length = len(data)
+    socket.sendall(struct.pack('!I', length))
+    socket.sendall(data)
+
+def receiveAll(socket, count):
+    buf = b''
+    while count:
+        newbuf = socket.recv(count)
+        if not newbuf: return None
+        buf += newbuf
+        count -= len(newbuf)
+    return buf
+
+def receiveOneMessage(socket):
+    #El length esta empacado en exactamente 4 bytes.
+    lengthBuf = receiveAll(socket, 4)
+    length, = struct.unpack('!I', lengthBuf)
+    return receiveAll(socket, length)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
@@ -16,36 +38,61 @@ try:
     print('Se ha establecido una conexion exitosa con el servidor.')
 except:
     print('No se ha podido establecer una conexion con el servidor.')
+    s.close()
     exit()
 
-
 #Se envia un mensaje inicial al servidor
-mensajeInicial = BEG_RECV
-s.send((mensajeInicial))
-print('Enviando comando: ', repr(mensajeInicial))
+sendOneMessage(s, BEG_RECV)
+print('Enviando comando: ', repr(BEG_RECV))
 print('El cliente esta listo para recibir archivos.')
 
-mensajeConfirmacion = s.recv(BUFFER_SIZE)
+mensajeConfirmacion = receiveOneMessage(s)
 if(repr(mensajeConfirmacion) != repr(OK)):
     print(repr(mensajeConfirmacion))
     print('No se ha recibido el mensaje %s esperado, finalizando conexion' %(repr(OK)))
+    err = receiveOneMessage(s)
+    print('Comando recibido: ', repr(err))
+    s.close()
     exit()  
 print('Comando recibido: ', repr(mensajeConfirmacion))
 
-mensajeOpcionesArchivos = s.recv(BUFFER_SIZE)
+mensajeOpcionesArchivos = receiveOneMessage(s)
 if(repr(mensajeOpcionesArchivos) != repr(FILE_OPTS)):
     print(repr(mensajeOpcionesArchivos))
     print('No se ha recibido el mensaje %s esperado, finalizando conexion' %(repr(FILE_OPTS)))
+    err = receiveOneMessage(s)
+    print('Comando recibido: ', repr(err))
+    s.close()
     exit()  
 print('Comando recibido: ', repr(mensajeOpcionesArchivos))
 
-print('Escoja el numero de archivo a enviar')
-mensajeNombreArchivo = s.recv(BUFFER_SIZE)
+print('Los archivos disponibles en el servidor son: ')
+mensajeNombreArchivo = receiveOneMessage(s)
 numArchivo = 1
 while (mensajeNombreArchivo != FILENAMES_SENT):
-    print(str(numArchivo) + '. ' + repr(mensajeNombreArchivo) + '\n')  
-    mensajeNombreArchivo = s.recv(BUFFER_SIZE)
+    print(str(numArchivo) + '. ' + repr(mensajeNombreArchivo))  
+    mensajeNombreArchivo = receiveOneMessage(s)
     numArchivo+=1
+print('Comando recibido: ', repr(mensajeNombreArchivo))
+#Se resta 1 porque vamos a la posicion del arreglo en que se encuentra el archivo en el servidor
+try:
+    archivoElegido = str(int(input('Escoja el numero de archivo a recibir: \n')) - 1)
+    sendOneMessage(s, archivoElegido.encode())
+except:
+    print('Opcion incorrecta. Cerrando comunicacion con el servidor.')
+    s.close()
+    exit()
+
+confirmacionArchivoElegido = receiveOneMessage(s)
+if(repr(confirmacionArchivoElegido) != repr(OK)):
+    print('Comando recibido: ', repr(confirmacionArchivoElegido))
+    print('Error. Cerrando la conexion con el servidor.')
+    s.close()
+    exit()
+
+print('Comando recibido: ', repr(confirmacionArchivoElegido))
+print('El servidor ha verificado el nombre del archivo.')
+print('Comenzando transferencia de datos...')
 
 """
 with open('recibido.txt', 'wb') as f:
--- Servidor/ClientHandler.py
+++ Servidor/ClientHandler.py
@@ -1,14 +1,16 @@
 import socket
 from threading import Thread
 from socketserver import ThreadingMixIn
+import struct
 
 BUFFER_SIZE = 1024
 BEG_RECV = b'BEG_RECV'
 OK = b'OK'
 FILE_OPTS = b'FILE_OPTS'
 FILENAMES_SENT = b'FILENAMES_SENT'
+ERR = b'ERR'
 
-files = [b'./files/prueba1.txt', b'./files/prueba2.txt']
+files = ['./files/prueba1.txt', './files/prueba2.txt']
 
 class ClientHandler(Thread):
     def __init__(self, ip, port, sock):
@@ -17,25 +19,60 @@ class ClientHandler(Thread):
         self.port = port
         self.sock = sock
         print('Iniciando un nuevo thread para ' + str(ip) +':' + str(port))
+
+    def sendOneMessage(self, data):
+        length = len(data)
+        self.sock.sendall(struct.pack('!I', length))
+        self.sock.sendall(data)
+
+    def receiveAll(self, count):
+        buf = b''
+        while count:
+            newbuf = self.sock.recv(count)
+            if not newbuf: return None
+            buf += newbuf
+            count -= len(newbuf)
+        return buf
+
+    def receiveOneMessage(self):
+        lengthBuf = self.receiveAll(4)
+        length, = struct.unpack('!I', lengthBuf)
+        return self.receiveAll(length)
     
     def run(self):
-        notifBeginCliente = self.sock.recv(BUFFER_SIZE)
+        notifBeginCliente = self.receiveOneMessage()
         if (repr(notifBeginCliente) != repr(BEG_RECV)):
             print('El mensaje recibido es incorrecto. Finalizando conexion con: ', (self.ip, self.port))
+            self.sendOneMessage(ERR)
             return
         print('Comando recibido: ', repr(notifBeginCliente))  
 
         print('Enviando comando: ', OK)
-        self.sock.send(OK)
+        self.sendOneMessage(OK)
         print('El servidor esta listo para enviar archivos.')
 
         print('Enviando comando: ', FILE_OPTS)
-        self.sock.send(FILE_OPTS)
+        self.sendOneMessage(FILE_OPTS)
         print('Enviando nombres de archivos al cliente')
+
         for opt in files:
-            self.sock.send(opt)
-        self.sock.send(FILENAMES_SENT)
+            self.sendOneMessage(opt.encode())
+
+        self.sendOneMessage(FILENAMES_SENT)
         print('Envio de nombres de archivos finalizado')
+
+        try:
+            archivoElegido = int(repr(self.receiveOneMessage())[2:-1])
+            print('Se ha elegido el archivo: ' + repr(files[archivoElegido]))
+            self.sendOneMessage(OK)
+        except:
+            print('Archivo no disponible. Cerrando la conexion con: ', (self.ip, self.port))
+            self.sendOneMessage(ERR)
+            return
+
+
+
+
 
 """
         filename = 'data/prueba.txt'
